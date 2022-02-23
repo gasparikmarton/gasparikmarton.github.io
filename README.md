b33605bfefd42dc87c381cf7eb6420456323bab6
gasparikmarton <gasparikmarton@gmail.com>
2022-02-23 15:32:35 +0100

    intersected_buggos

---
+++ README.md
@@ -0,0 +1,37 @@
+# TBAG Final Project - *Fly*
+
+## Project Link
+
+
+[Link to project](https://gasparikmarton.github.io/fly/)
+
+This project is inspired by the phenomena of ferrofluids and is all about tension, about how borders give shape and how breaking through those borders enables to form new shapes or merge with others. Audio reactive visuals can either simply be enjoyed or interactively be manipulated. 
+
+### [Link to Video](https://youtu.be/t2Bq1nFchSA)](https://youtu.be/qsh5A_-NutM)
+Partially inspired by an [nft by Polyhop](https://www.instagram.com/p/CYZcY7aownZ/).
+
+## Technical Choices
+
+### Sound
+
+In the last couple of years I've spent a lot of time listening and digging various genres of electronic music. Hopefully, there is still much to discover, but so far, my favourite kind of sound was this "retro-synth-trancy-house". 
+My only goal with this project was to try to recreate that kind of vibe, these songs can transmit, using a classic synthesizer, and doing the drum-coding all by myself (without loop samples). 
+
+There could still be a lot to work on, but overall, I had a very good time in Ableton. After the study sessions, I felt, that I finally have an idea about what I'm doing, and my workflow was much more goal-oriented then before.
+
+### Graphics
+
+From the very first moment, when I learned about Three.js, I knew that I want to discover this environment. It really reminded me of the node-based real-time visual engines, unlike p5 or Processing, which is closer to my heart.
+
+My goal was to create a video-clip-like scene, where I use keyframing as well as sound-reactive parameters, and also implement post-processing. I also wanted to use some GLSL, but I felt that I needed more time with that, and really wanted to just have some less painful visual programming experience, which I could achieve after some days of pain and rage. 
+
+## Artistic Intention
+
+I really focused on the visuals, and a kind of a storyline recreating a dreamy state.
+
+## Learnings and Reflections
+
+A bit unfortunate, that I didn't have the time to polish it, but that's my only negative thought about the project. 
+
+
+Even though it was my first complex programming task, it was quite challanging, but just as rewarding at some points. Honestly, I don't remember entering such a flow while "working" in the past few years. I'm glad that I've learned to google and debug properly, as well as to implement own ideas. 
---
+++ scene.js
@@ -0,0 +1,359 @@
+import { FontLoader } from './lib/FontLoader.js';
+import { VertexNormalsHelper } from './lib/VertexNormalsHelper.js';
+import { TextGeometry } from './lib/TextGeometry.js';
+import { Vector3 } from './lib/three.module.js';
+
+// SETUP
+let camera, scene, pointLight, renderer, stats;
+let fov, dist;
+let perspWidth, perspHeight;
+let pointer = new THREE.Vector2();
+let clock = new THREE.Clock;
+
+// BOXES
+let framingSize = 50;
+let rowSize = 30;
+let rectWidth = 0;
+let columnSize = 0;
+
+let buffTest;
+let boxes;
+let numOfBoxes;
+
+// INTERSECTION
+let raycaster;
+let INTERSECTED;
+
+
+// UPDATE
+function updateBoxes() { };
+
+
+
+init();
+animate();
+
+function init() {
+
+    // SCENE
+    scene = new THREE.Scene();
+    // 
+
+    // CAM
+    fov = 60;
+    dist = 300;
+
+    camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = dist;
+
+
+    var vFOV = THREE.MathUtils.degToRad(fov); // convert vertical fov to radians
+    perspHeight = 2 * Math.tan(vFOV / 2) * dist; // visible height
+    perspWidth = perspHeight * camera.aspect;           // visible width
+    // 
+
+    //LIGHT
+    var ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+    pointLight = new THREE.PointLight(0xc9efff, 0.5);
+
+    pointLight.castShadow = true;
+    pointLight.shadow.mapSize.width = 512;
+    pointLight.shadow.mapSize.height = 512;
+    pointLight.shadow.camera.near = 0.5;
+    pointLight.shadow.camera.far = 600;
+    scene.add(pointLight, ambientLight);
+
+    const sphereSize = 1;
+    const pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
+    scene.add(pointLightHelper);
+    // 
+
+    const size = 1000;
+    const divisions = 10;
+
+
+
+
+    // RENDERER
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setClearColor("#000000");
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // renderer.setSize(192 * 5, 108 * 5);
+
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    // 
+
+
+
+    let colors = [0xFF0000, 0xFFFF00, 0x0000FF, 0xFFFFFF];
+
+
+    function planar() {
+        let mininmumScale = 40;
+        let scale = 126;
+        let boxPositions = [];
+        let boxScales = [];
+        let boxColors = [];
+        let color;
+
+
+        for (let posY = Math.floor(perspHeight / 2); posY >= -perspHeight / 2 + columnSize / 2; posY -= columnSize) {
+            columnSize = Math.ceil(Math.random() * scale / 10) * 20 + mininmumScale;
+            if (posY - columnSize < -perspHeight / 2) {
+                columnSize = -1 * (-perspHeight / 2 + posY)
+            }
+
+            for (let posX = Math.floor(0 - perspWidth / 2); posX <= perspWidth / 2 - rectWidth / 2; posX += rectWidth) {
+                console.log("cS", columnSize);
+
+                rectWidth = Math.ceil(Math.random() * scale / 10) * 20 + mininmumScale;
+
+                boxColors.push(colors[Math.floor(Math.random() * colors.length)]);
+
+                boxPositions.push(new THREE.Vector3(posX + rectWidth / 2, posY - columnSize / 2, 0));
+                boxScales.push(new THREE.Vector3(rectWidth * 0.99, columnSize * 0.99, 50));
+                // boxScales.push(new THREE.Vector3(1, 1, 1));
+            }
+
+        }
+        numOfBoxes = boxPositions.length;
+        // GEO
+        buffTest = new THREE.BufferGeometry();
+
+        let x = 0.5;
+        let y = 0.5;
+        let z = 0.5;
+
+
+        const vertices = new Float32Array([
+            // back
+            -x, -y, -z,
+            x, -y, -z,
+            x, y, -z,
+
+            x, y, -z,
+            -x, y, -z,
+            -x, -y, -z,
+
+            // left
+            -x, y, z,
+            -x, -y, -z,
+            -x, y, -z,
+
+            -x, y, z,
+            -x, -y, z,
+            -x, -y, -z,
+
+
+            // right
+            x, -y, -z,
+            x, -y, z,
+            x, y, z,
+
+            x, -y, -z,
+            x, y, z,
+            x, y, -z,
+
+            // top
+            -x, y, -z,
+            x, y, -z,
+            x, y, z,
+
+            -x, y, -z,
+            x, y, z,
+            -x, y, z,
+
+            // bottom
+            -x, -y, -z,
+            -x, -y, z,
+            x, -y, z,
+
+            x, -y, -z,
+            -x, -y, -z,
+            x, -y, z,
+
+
+
+        ]);
+        buffTest.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+
+        const box = new THREE.BoxGeometry(1, 1, 1);
+
+
+        // MESH
+        boxes = new Array(numOfBoxes)
+            .fill(null)
+            .map(() => new THREE.Mesh(buffTest, new THREE.MeshLambertMaterial({})));
+
+        boxes.forEach((x, i) => {
+            x.castShadow = true;
+            x.receiveShadow = true;
+
+            x.material.color.set(boxColors[i]);
+
+            scene.add(x);
+
+            x.position.set(boxPositions[i].x, boxPositions[i].y, boxPositions[i].z);
+            x.scale.set(boxScales[i].x, boxScales[i].y, boxScales[i].z);
+
+            x.geometry.computeVertexNormals();
+            x.geometry.normalizeNormals();
+
+            let vnh = new VertexNormalsHelper(x, 50);
+            scene.add(vnh);
+
+        });
+
+
+        // updateBoxes = (t) => { boxes.forEach((x, i) => { x.rotation.y = t * (Math.PI / 2) }) }
+
+        // console.log("posX=" + posX);
+        console.log("rectWidth=" + rectWidth);
+        console.log("boxPos=", boxPositions);
+        console.log("boxScale=", boxScales);
+        console.log("boxColors", boxColors);
+
+
+        console.log("boxes=", boxes)
+        buffTest.computeVertexNormals();
+
+
+        raycaster = new THREE.Raycaster();
+
+    };
+
+
+    planar();
+
+
+    function spherer(x, y) {
+        const geometry = new THREE.SphereGeometry(6, 32, 6);
+        const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        const sphere = new THREE.Mesh(geometry, material);
+        scene.add(sphere);
+        sphere.position.set(x, y, 20);
+    }
+    spherer(0, 0);
+    spherer(-perspWidth / 2, perspHeight / 2)
+
+    animate();
+
+
+
+    // Append Renderer to DOM
+    window.addEventListener('resize', onWindowResize);
+    document.body.appendChild(renderer.domElement);
+    document.addEventListener('pointermove', onPointerMove);
+
+};
+
+
+
+
+
+
+
+
+function animate() {
+
+    requestAnimationFrame(animate);
+
+    raycaster.setFromCamera(pointer, camera);
+
+    const intersects = raycaster.intersectObjects(boxes, false);
+
+    if (intersects.length > 0) {
+
+
+
+        intersects[0].object.material.emissive.setHex(0xff0000);
+        // if (INTERSECTED != intersects[0].object) {
+
+        //     if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+
+        //     INTERSECTED = intersects[0].object;
+        //     INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+        //     INTERSECTED.material.emissive.setHex(0xff0000);
+
+        console.log("inter=", intersects.length)
+
+        // }
+    } else {
+
+
+        // intersects[0].object.material.emissive.setHex(0xfff000);
+
+        // if (INTERSECTED) {
+        //     INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+
+        //     INTERSECTED = null;
+
+        // }
+
+    };
+
+    // };
+
+    render();
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+// Render Loop
+function render() {
+    requestAnimationFrame(render);
+    // pointLight.position.set(pointer.x, pointer.y, 0);
+
+
+
+
+
+    // Render the scene
+    renderer.render(scene, camera);
+};
+
+// render();
+
+
+function onPointerMove(event) {
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+    pointLight.position.set(pointer.x * perspWidth / 2, pointer.y * perspHeight / 2, 50);
+}
+
+
+
+function loadFont() {
+
+    const loader = new FontLoader();
+    loader.load('fonts/' + fontName + '_' + fontWeight + '.typeface.json', function (response) {
+
+        font = response;
+
+        refreshText();
+
+    });
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
